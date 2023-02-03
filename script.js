030abd0d8429a69498a1b2eb97cca57a295ae94b
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-02-03 20:40:31 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,89 @@
+let playerName = prompt('Your Name')
+let playerScore = 0
+let computerScore = 0
+let roundWinner = '';
+
+console.log(`Computer Choice (${getComputerChoice()})`)
+console.log(`${playerName}'s Score (${playerScore})`);
+console.log(`Ai's Score (${computerScore})`);
+console.log(roundWinner);
+
+
+
+// function getComputerChoice() {
+//     const selection = ['ROCK', 'PAPER', 'SCISSORS'];
+//     return selection[Math.floor(Math.random() *3) ];
+    
+// }
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+      case 0:
+        return 'ROCK'
+      case 1:
+        return 'PAPER'
+      case 2:
+        return 'SCISSORS'
+    }
+}
+
+
+
+
+
+function playRound(playerSelection,computerSelection) {
+
+
+
+    if (computerSelection === playerSelection) {
+        roundWinner = 'Draw'
+    }
+
+    if (
+        (playerSelection == 'ROCK' && computerSelection == 'SCISSORS' ) ||
+        (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') ||
+        (playerSelection =='PAPER' && computerSelection  == 'ROCK')
+    )
+    {
+        playerScore++
+        roundWinner = `${playerName}`
+    }
+
+    if (
+        (computerSelection == 'ROCK' && playerSelection == 'SCISSORS' ) ||
+        (computerSelection == 'SCISSORS' && playerSelection == 'PAPER') ||
+        (computerSelection == 'PAPER' && playerSelection  == 'ROCK')
+    )
+    {
+        computerScore++
+        roundWinner = 'Ai'
+    }
+
+    return;
+}
+
+// function game() {
+
+//     return ;
+// }
+
+function isGameOver(){
+    return playerScore === 5 || computerScore === 5;
+}
+
+alert(playRound(prompt("your game"), getComputerChoice()));
+alert(prompt("your game"))
+
+
+
+// function game() {
+
+// return;
+// }
+
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+
+// console.log(playRound(playerSelection, computerSelection))
