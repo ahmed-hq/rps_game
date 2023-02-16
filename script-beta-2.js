4d4389056d7df31d56892685b7191773d687be5f
Ahmed Walid <123060771+ahmed-hq@users.noreply.github.com>
2023-02-16 16:15:58 +0200

    Finish basic functions to play game rounds

--- script-beta-2.js
+++ script-beta-2.js
@@ -5,6 +5,20 @@ let gameResult;
 
 // Start the game
 // Play Round
+function playRound() {
+  let playerChoice = getPlayerChoice();
+  let computerChoice = getComputerChoice();
+  let roundWinner = calcRoundWinner(playerChoice, computerChoice);
+  console.log(playerChoice, computerChoice);
+  console.log(roundWinner);
+}
+for (let i = 1; i <= 5; i++) playRound(i);
+
+function getComputerChoice() {
+  let choices = ["rock", "paper", "scissors"];
+  return choices[Math.floor(Math.random() * 3)];
+}
+
 function getPlayerChoice() {
   loop: while (true) {
     let playerChoice = prompt("Rock - Paper - Scissors").toLowerCase();
@@ -21,16 +35,22 @@ function getPlayerChoice() {
   }
 }
 
-function getComputerChoice() {
-  let choices = ["rock", " paper", "scissors"];
-  return choices[Math.floor(Math.random() * 3)];
+function calcRoundWinner(playerChoice, computerChoice) {
+  if (
+    (computerChoice == "rock" && playerChoice == "scissors") ||
+    (computerChoice == "paper" && playerChoice == "rock") ||
+    (computerChoice == "scissors" && playerChoice == "paper")
+  ) {
+    return "computer";
+  } else if (
+    (playerChoice == "rock" && computerChoice == "scissors") ||
+    (playerChoice == "paper" && computerChoice == "rock") ||
+    (playerChoice == "scissors" && computerChoice == "paper")
+  ) {
+    return "player";
+  } else {
+    return "tie";
+  }
 }
 
-function playRound() {
-  let roundWinner;
-}
-
-// Repeat 5 times
-for (let i = 1; i <= 5; i++) playRound(i);
-
-console.log(getPlayerChoice());
+console.log();
